34fc5252912f73c0dfd73193442a0bb0257fe1e9
Andrew huggins <andrewhuggins@gmail.com>
2018-07-03 00:46:50 -0400

    show users on the site home page...if not logged in show marketing frontpage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(AuthorizationCheckerInterface $authChecker)
     {
-        return $this->render('home/index.html.twig', [
+        if ($authChecker->isGranted('ROLE_USER') === true) {
+            $repository = $this->getDoctrine()->getRepository(User::class);
+            return $this->render('home/index.html.twig', [
+                'users' => $repository->findAll(),
+                'controller_name' => 'HomeController',
+            ]);
+        }
+        return $this->render('frontpage/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
