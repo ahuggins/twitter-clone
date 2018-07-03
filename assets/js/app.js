28a2564fb8b90c4eff5b625bb6aec943b7e46ca6
Andrew huggins <andrewhuggins@gmail.com>
2018-07-03 00:44:04 -0400

    Register components, create Vue instance, add EventBus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-// require('../css/style.css');
+import Vue from 'vue';
+ 
+import Tweeter from './components/Tweeter'
+import TweetList from './components/TweetList'
+ 
+Vue.prototype.$eventBus = new Vue()
+
+new Vue({
+  el: '#app',
+  components: {Tweeter, TweetList}
+});
