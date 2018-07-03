022d119f4e0b62d8b29771047134efb3aa72a4b1
Andrew huggins <andrewhuggins@gmail.com>
2018-07-03 00:48:02 -0400

    view profile of user and special route for logged in user profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Security;
 
 class ProfileController extends Controller
 {
     /**
-     * @Route("/profile/{username}", name="profile")
+     * @Route("/user/{username}", name="userProfile")
      */
-    public function show($username)
+    public function userProfile($username)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneBy(['username' => $username]);
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    
+    /**
+     * @Route("/profile", name="loggedInProfile")
+     */
+    public function show(Security $security)
     {
         return $this->render('profile/index.html.twig', [
+            'user' => $security->getUser(),
             'controller_name' => 'ProfileController',
         ]);
     }
